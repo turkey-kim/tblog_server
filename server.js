484785efbecdf18a52408e269f715bfcaa9dc47d
turkey-kim <rlaxmrgml@naver.com>
2023-07-07 03:10:20 +0900

    feat: 대댓글 API 서버 연동

--- server.js
+++ server.js
@@ -22,6 +22,10 @@ const postComment = require("./api/postComment.js");
 const getComments = require("./api/getComments.js");
 const deleteComment = require("./api/deleteComment.js");
 const editComment = require("./api/editComment.js");
+const postRecomment = require("./api/postRecomment.js");
+const getRecomment = require("./api/getRecomment.js");
+const deleteRecomment = require("./api/deleteRecomment.js");
+const editRecomment = require("./api/editRecomment.js");
 ("");
 
 // 토큰은 로그인 시에만 발급한다. 특히, 페이로드에는 아이디 정보가 들어가기 때문에, 발급 시점은 로그인 시점이다.
@@ -54,6 +58,10 @@ app.use("/", postComment);
 app.use("/", getComments);
 app.use("/", deleteComment);
 app.use("/", editComment);
+app.use("/", postRecomment);
+app.use("/", getRecomment);
+app.use("/", deleteRecomment);
+app.use("/", editRecomment);
 
 async function startServer() {
   try {
