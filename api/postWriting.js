bef0ceddc1f8fada5ccf1f4f8992911abc38161a
TeukHee <rlaxmrgml@naver.com>
2024-01-01 23:34:33 +0900

    feat: 글 생성 api 에러처리

--- api/postWriting.js
+++ api/postWriting.js
@@ -17,13 +17,13 @@ router.post("/api/post_writing", async (req, res) => {
       auth: req.body.auth,
     });
 
-    db.collection("counter").updateOne(
-      { name: "게시글개수" },
-      { $inc: { total: 1 } }
-    );
+    await db
+      .collection("counter")
+      .updateOne({ name: "게시글개수" }, { $inc: { total: 1 } });
+    res.status(200).json({ success: true, message: "글 발행완료" });
   } catch (error) {
     console.error(error);
-    throw new Error(error);
+    res.status(500).json({ success: false, message: "서버에러" });
   }
 });
 
